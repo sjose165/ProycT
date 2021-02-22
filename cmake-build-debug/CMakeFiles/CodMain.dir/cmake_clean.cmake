391aae3c1525503691d1ef794d05a1061e555dd8
sjose165 <187360@iberopuebla.mx>
2021-02-21 23:44:03 -0600

    Todos los archivos subidos

---
+++ cmake-build-debug/CMakeFiles/CodMain.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CodMain.dir/main.cpp.obj"
+  "CodMain.exe"
+  "CodMain.exe.manifest"
+  "CodMain.pdb"
+  "libCodMain.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CodMain.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/CodMain.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for CodMain.
+# This may be replaced when dependencies are built.
